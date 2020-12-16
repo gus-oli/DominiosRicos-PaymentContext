104b922322cf66b837020c1e795b0f1e6a2da02e
Gustavo <gustavo.silva1995@outlook.com>
2020-12-15 22:03:37 -0300

    Refatoração das entidades, criação de tipos especiais, criação dos ValueObjects e Contracts

--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -1,13 +1,47 @@
 using System;
+using System.Collections.Generic;
+using Flunt.Validations;
+using PaymentContext.Shared.Entities;
 
 namespace PaymentContext.Domain.Entities
 {
-    public class Subscription
+    public class Subscription : Entity
     {
-        public DateTime CreateDate { get; set; }
-        public DateTime LastUpdateDate { get; set; }
-        public DateTime? ExpireDate { get; set; }
-        public bool Active { get; set; }
-        public List<Payment> Payments { get; set; }
+        private IList<Payment> _payments;
+        public Subscription(DateTime? expireDate)
+        {
+            CreateDate = DateTime.Now;
+            LastUpdateDate = DateTime.Now;
+            ExpireDate = expireDate;
+            Active = true;
+            _payments = new List<Payment>();
+        }
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
+        public bool Active { get; private set; }
+        public IReadOnlyCollection<Payment> Payments { get; private set; }
+
+        public void AddPayment(Payment payment)
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .IsGreaterThan(DateTime.Now, payment.PaidDate, "Subscrption.Payments", "A data do pagamento deve ser futura")
+            );
+            
+            _payments.Add(payment);
+        }
+
+        public void Activate()
+        {
+            Active = true;
+            LastUpdateDate = DateTime.Now;
+        }
+        public void Deactivate()
+        {
+            Active = false;
+            LastUpdateDate = DateTime.Now;
+        }
     }
 }
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            /*var name = new Name("Teste", "Teste");
+            foreach (var not in name.Notifications)
+            {
+                not.Message;
+            }*/
+        }
+    }
+}
